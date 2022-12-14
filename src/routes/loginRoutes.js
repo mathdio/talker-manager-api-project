5efde7d19516645d4bf9b64e0a8782a96601d724
matheus <mathdiogenes@gmail.com>
2022-12-14 16:19:00 -0300

    feat: Requisito 4

--- src/routes/loginRoutes.js
+++ src/routes/loginRoutes.js
@@ -5,12 +5,23 @@ const generateToken = require('../utils/generateToken');
 const router = express.Router();
 
 router.post('/', (req, res) => {
-  // const { name, email } = req.body;
+  const { email, password } = req.body;
+  const emailRegex = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
 
-  // if (![name, email].includes(undefined)) {
+  if (!email) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+  if (!password) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (!emailRegex.test(email)) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
     const token = generateToken();
-
     return res.status(200).json({ token });
-});
+  });
 
 module.exports = router;
